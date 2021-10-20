fac088d77a4e2ab663c8c67f1a453535876a1511
Brook Tarekegn <brooksideas@gmail.com>
2021-10-21 00:12:43 +0300

    Accessiability improvement for underGrad.h

--- underGrad.h
+++ underGrad.h
@@ -1,24 +1,32 @@
 #ifndef underGrad_h
 #define underGrad_h
-#include<string>
-#include<iostream>
-#include"student.h"
+#include <string>
+#include <iostream>
+#include "student.h"
 using namespace std;
-class underGrad:public student
-{ 
+class underGrad : public student
+{
+protected:
+    enum sStat
+    {
+        PROBATION,
+        GOOD,
+        SPECIAL,
+        NONE
+    };
     
-    enum sStat{PROBATION, GOOD, SPECIAL, NONE};
-    private:
+private:
     string advisor = "";
     sStat sStatus = NONE;
 
-    public: 
-    underGrad (std::string="", std::string="", std::string="",std::string="", double=0.0, double=0.0,double=0.0, std::string="", sStat=NONE);
-    string getAdvisor()const;
-    sStat getStatus()const;
+
+
+public:
+    underGrad(std::string = "", std::string = "", std::string = "", std::string = "", double = 0.0, double = 0.0, double = 0.0, std::string = "", sStat = NONE);
+    string getAdvisor() const;
+    sStat getStatus() const;
     void setAdvisor(string);
     void setStatus(sStat);
     void showStudent();
-    
 };
 #endif
